a0ce650079afbccaca36b7a2271d06c23eb97d36
Salony Gupta <54049147+lonylitical@users.noreply.github.com>
2025-11-15 17:25:10 +0530

    Add files via upload

--- DemoJdbc.java
+++ DemoJdbc.java
@@ -17,15 +17,12 @@ close
 
 */
 
-        int sid=101;
-        String sname="Max";
-        int marks=48;
-
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "6031";
-
-        String sql = "insert into student values ("+sid+",'"+sname+"',"+marks+")";
+       // String sql = "insert into student values (5, 'John', 48)";
+       // String sql = "update student set sname = 'Max' where sid = 5";
+        String sql = "delete from student where sid = 5";
         // Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("Connection established");
